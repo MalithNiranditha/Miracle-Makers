2c68e13bda5ab2d1445757d862ad65f0107f9f54
Malith99-06 <malithugc13@gmail.com>
2022-05-10 23:53:53 +0530

    created done

---
+++ app/src/main/java/com/example/miraclemakers/Activities/Customer/DoneActivity.java
@@ -0,0 +1,18 @@
+package com.example.miraclemakers.Activities.Customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.miraclemakers.R;
+
+public class DoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_done);
+
+
+    }
+}
